1d683a606b025bc2ec960aee188cfd5f30d696fc
JeongNoa <38281010+JeongNoa@users.noreply.github.com>
2019-06-08 13:51:33 +0900

    Update GAN.py

--- Research_Code/GAN.py
+++ Research_Code/GAN.py
@@ -1,197 +1,173 @@
-import keras
-from keras import layers
-import numpy as np
-from PIL import Image
-import glob
+from __future__ import print_function, division
 
-#location = 'C:\Users\JeongNoa\Documents\과제연구\data\Data_Extracted'
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
 
-tot_list = [] 
-#aniname = int(input())
-
-ani_num = {0:'공의 경계', 1: '너의 이름은', 2: '문호 스트레이 독스', 3: '바이올렛 에버가든', 4: '빈란드 사가', 
-           5: '언어의 정원', 6: '원펀맨', 7: '일상', 8: '작안의 샤나', 9: '잔향의 테러', 10: '진격의 거인', 11: '페그오 X 공의 경계'}
-
-#aniname = location + '\\' + ani_num[aniname]
-
-aniname = 'C:\\Users\\JeongNoa\\Documents\\과제연구\\data\\Data_Extracted_2\\공의 경계'
-print(aniname)
-
-temp = 0
-for foldername in glob.glob(aniname + '/*'):
-    for filename in glob.glob(foldername + '/*jpg'): 
-        im = Image.open(filename) 
-        arr = np.array(im) 
-        tot_list.append(arr) 
-        if temp == 29:
-            break
-        temp += 1
-    if temp == 29:
-        break
-		
-tot_list = np.asarray(tot_list)
-print(np.shape(tot_list))
-
-latent_dim = 32
-height = 120
-width = 214
-channels = 3
-
-generator_input = keras.Input(shape=(latent_dim,))
-
-
-
-# 입력을 16 × 16 크기의 128개 채널을 가진 특성 맵으로 변환합니다
-x = layers.Dense(128 * 60 * 107)(generator_input)
-x = layers.LeakyReLU()(x)
-x = layers.Reshape((60, 107, 128))(x)
-
-# 합성곱 층을 추가합니다
-x = layers.Conv2D(256, 5, padding='same')(x)
-x = layers.LeakyReLU()(x)
-
-# 32 × 32 크기로 업샘플링합니다
-x = layers.Conv2DTranspose(256, 4, strides=2, padding='same')(x)
-x = layers.LeakyReLU()(x)
-
-# 합성곱 층을 더 추가합니다
-x = layers.Conv2D(256, 5, padding='same')(x)
-x = layers.LeakyReLU()(x)
-x = layers.Conv2D(256, 5, padding='same')(x)
-x = layers.LeakyReLU()(x)
-
-# 32 × 32 크기의 1개 채널을 가진 특성 맵을 생성합니다
-x = layers.Conv2D(channels, 7, activation='tanh', padding='same')(x)
-generator = keras.models.Model(generator_input, x)
-generator.summary()
-
-
-
-discriminator_input = layers.Input(shape=(height, width, channels))
-x = layers.Conv2D(128, 3)(discriminator_input)
-x = layers.LeakyReLU()(x)
-x = layers.Conv2D(128, 4, strides=2)(x)
-x = layers.LeakyReLU()(x)
-x = layers.Conv2D(128, 4, strides=2)(x)
-x = layers.LeakyReLU()(x)
-x = layers.Conv2D(128, 4, strides=2)(x)
-x = layers.LeakyReLU()(x)
-x = layers.Flatten()(x)
-
-# 드롭아웃 층을 넣는 것이 아주 중요합니다!
-x = layers.Dropout(0.4)(x)
-
-# 분류 층
-x = layers.Dense(1, activation='sigmoid')(x)
-
-discriminator = keras.models.Model(discriminator_input, x)
-discriminator.summary()
-
-# 옵티마이저에서 (값을 지정하여) 그래디언트 클리핑을 사용합니다
-# 안정된 훈련을 위해서 학습률 감쇠를 사용합니다
-discriminator_optimizer = keras.optimizers.RMSprop(lr=0.0008, clipvalue=1.0, decay=1e-8)
-discriminator.compile(optimizer=discriminator_optimizer, loss='binary_crossentropy')
-
-# 판별자의 가중치가 훈련되지 않도록 설정합니다(gan 모델에만 적용됩니다)
-discriminator.trainable = False
-
-gan_input = keras.Input(shape=(latent_dim,))
-gan_output = discriminator(generator(gan_input))
-gan = keras.models.Model(gan_input, gan_output)
-
-gan_optimizer = keras.optimizers.RMSprop(lr=0.0004, clipvalue=1.0, decay=1e-8)
-gan.compile(optimizer=gan_optimizer, loss='binary_crossentropy')
-
-
-import os
-from keras.preprocessing import image
-
-# CIFAR10 데이터를 로드합니다
-x_train = tot_list                #완료
-
-# 데이터를 정규화합니다
-x_train = x_train.reshape(
-    (x_train.shape[0],) + (height, width, channels)).astype('float32') / 255.
-
-iterations = 10000
-batch_size = 20
-save_dir = 'C:\\Users\\JeongNoa\\Documents\\과제연구\\gan_sample'
-if not os.path.exists(save_dir):
-    os.mkdir(save_dir)
-	
-labels = np.concatenate([np.ones((batch_size, 1)),
-                             np.zeros((batch_size, 1))])
-print(np.shape(labels))
-
-# 훈련 반복 시작
-start = 0
-for step in range(iterations):
-    # 잠재 공간에서 무작위로 포인트를 샘플링합니다
-    random_latent_vectors = np.random.normal(size=(batch_size, latent_dim))
-
-    # 가짜 이미지를 디코딩합니다
-    generated_images = generator.predict(random_latent_vectors)
-
-    # 진짜 이미지와 연결합니다
-    stop = start + batch_size
-    real_images = x_train[start: stop]
-    combined_images = np.concatenate([generated_images, real_images])
-    
-    # 진짜와 가짜 이미지를 구분하여 레이블을 합칩니다
-    labels = np.concatenate([np.ones((batch_size, 1)),
-                             np.zeros((batch_size, 1))])
-    # 레이블에 랜덤 노이즈를 추가합니다. 아주 중요합니다!
-    labels += 0.05 * np.random.random(labels.shape)
-
-    # discriminator를 훈련합니다
-    d_loss = discriminator.train_on_batch(combined_images, labels)
-
-    
-    
-    # 잠재 공간에서 무작위로 포인트를 샘플링합니다
-    random_latent_vectors = np.random.normal(size=(batch_size, latent_dim))
-
-    # 모두 “진짜 이미지"라고 레이블을 만듭니다
-    misleading_targets = np.zeros((batch_size, 1))
-
-    # generator를 훈련합니다(gan 모델에서 discriminator의 가중치는 동결됩니다)
-    a_loss = gan.train_on_batch(random_latent_vectors, misleading_targets)
-    
-    start += batch_size
-    if start > len(x_train) - batch_size:
-        start = 0
-
-    # 중간 중간 저장하고 그래프를 그립니다
-    if step % 100 == 0:
-        # 모델 가중치를 저장합니다
-        gan.save_weights('gan.h5')
-
-        # 측정 지표를 출력합니다
-        print('스텝 %s에서 판별자 손실: %s' % (step, d_loss))
-        print('스텝 %s에서 적대적 손실: %s' % (step, a_loss))
-
-        # 생성된 이미지 하나를 저장합니다
-        img = image.array_to_img(generated_images[0] * 255., scale=False)
-        img.save(os.path.join(save_dir, 'generated_frog' + str(step) + '.png'))
-
-        # 비교를 위해 진짜 이미지 하나를 저장합니다
-        img = image.array_to_img(real_images[0] * 255., scale=False)
-        img.save(os.path.join(save_dir, 'real_frog' + str(step) + '.png'))
-		
-        
 import matplotlib.pyplot as plt
 
-# 잠재 공간에서 랜덤한 포인트를 샘플링합니다
-random_latent_vectors = np.random.normal(size=(10, latent_dim))
+import sys
 
-# 가짜 이미지로 디코딩합니다
-generated_images = generator.predict(random_latent_vectors)
+import numpy as np
 
-for i in range(generated_images.shape[0]):
-    img = image.array_to_img(generated_images[i] * 255., scale=False)
-    plt.figure()
-    plt.imshow(img)
-    
-plt.show()
+class DCGAN():
+    def __init__(self):
+        # Input shape
+        self.img_rows = 28
+        self.img_cols = 28
+        self.channels = 1
+        self.img_shape = (self.img_rows, self.img_cols, self.channels)
+        self.latent_dim = 100
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy',
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # The generator takes noise as input and generates imgs
+        z = Input(shape=(self.latent_dim,))
+        img = self.generator(z)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated images as input and determines validity
+        valid = self.discriminator(img)
+
+        # The combined model  (stacked generator and discriminator)
+        # Trains the generator to fool the discriminator
+        self.combined = Model(z, valid)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+    def build_generator(self):
+
+        model = Sequential()
+
+        model.add(Dense(128 * 7 * 7, activation="relu", input_dim=self.latent_dim))
+        model.add(Reshape((7, 7, 128)))
+        model.add(UpSampling2D())
+        model.add(Conv2D(128, kernel_size=3, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Activation("relu"))
+        model.add(UpSampling2D())
+        model.add(Conv2D(64, kernel_size=3, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Activation("relu"))
+        model.add(Conv2D(self.channels, kernel_size=3, padding="same"))
+        model.add(Activation("tanh"))
+
+        model.summary()
+
+        noise = Input(shape=(self.latent_dim,))
+        img = model(noise)
+
+        return Model(noise, img)
+
+    def build_discriminator(self):
+
+        model = Sequential()
+
+        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+        model.add(ZeroPadding2D(padding=((0,1),(0,1))))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(1, activation='sigmoid'))
+
+        model.summary()
+
+        img = Input(shape=self.img_shape)
+        validity = model(img)
+
+        return Model(img, validity)
+
+    def train(self, epochs, batch_size=128, save_interval=50):
+
+        # Load the dataset
+        (X_train, _), (_, _) = mnist.load_data()
+
+        # Rescale -1 to 1
+        X_train = X_train / 127.5 - 1.
+        X_train = np.expand_dims(X_train, axis=3)
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random half of images
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            imgs = X_train[idx]
+
+            # Sample noise and generate a batch of new images
+            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            gen_imgs = self.generator.predict(noise)
+
+            # Train the discriminator (real classified as ones and generated as zeros)
+            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            # Train the generator (wants discriminator to mistake images as real)
+            g_loss = self.combined.train_on_batch(noise, valid)
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+
+            # If at save interval => save generated image samples
+            if epoch % save_interval == 0:
+                self.save_imgs(epoch)
+
+    def save_imgs(self, epoch):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
+        gen_imgs = self.generator.predict(noise)
+
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("images/mnist_%d.png" % epoch)
+        plt.close()
 
 
+if __name__ == '__main__':
+    dcgan = DCGAN()
+    dcgan.train(epochs=4000, batch_size=32, save_interval=50)
